4859b4bb59767b87ba0fb573ef2abb106554c5a9
Наталия Гаращенко <n.garashchenko@uplab.ru>
2024-04-27 18:43:05 +0300

    Исправлены баги в /install/index.php

--- lang/ru/install/index.php
+++ lang/ru/install/index.php
@@ -2,6 +2,6 @@
 
 $MESS["tryhardy.params_MODULE_NAME"] = "[Tryhardy] Params";
 $MESS["tryhardy.params_MODULE_DESCRIPTION"] = "Модуль для расширения возможностей настройки параметров компонентов";
-$MESS["tryhardy.params_MODULE_PARTNER_NAME"] = "Digital-агентство Uplab";
-$MESS["tryhardy.params_MODULE_PARTNER_URI"] = "https://www.uplab.ru";
+$MESS["tryhardy.params_MODULE_PARTNER_NAME"] = "Tryhardy";
+$MESS["tryhardy.params_MODULE_PARTNER_URI"] = "https://github.com/tryhardy";
 $MESS["tryhardy.params_MODULE_NO_D7_ERROR"] = "Ошибка! Данная версия 1С-Битрикс не поддерживается.";
