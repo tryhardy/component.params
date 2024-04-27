54ebf917b89467835382a06908e2cefb0d90512c
Наталия Гаращенко <n.garashchenko@uplab.ru>
2024-04-27 18:44:06 +0300

    Исправлены баги в /install/index.php

--- lang/en/install/index.php
+++ lang/en/install/index.php
@@ -2,6 +2,6 @@
 
 $MESS["tryhardy.params_MODULE_NAME"] = "[Tryhardy] Params";
 $MESS["tryhardy.params_MODULE_DESCRIPTION"] = "Tryhardy's Extended component params for easy developing";
-$MESS["tryhardy.params_MODULE_PARTNER_NAME"] = "Uplab digital agency";
-$MESS["tryhardy.params_MODULE_PARTNER_URI"] = "https://www.uplab.ru";
+$MESS["tryhardy.params_MODULE_PARTNER_NAME"] = "Tryhardy";
+$MESS["tryhardy.params_MODULE_PARTNER_URI"] = "https://github.com/tryhardy";
 $MESS["tryhardy.params_MODULE_NO_D7_ERROR"] = "Sorry. You are using unsupported 1C-Bitrix version.";
